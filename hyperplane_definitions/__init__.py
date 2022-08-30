20d7989c3fa395bf716dd6793e17a89df59658a6
tzvi_hyperplane <tzvi@hyperplane.cloud>
2022-08-30 15:37:13 +0300

    Add get_job_id to __init__

--- hyperplane_definitions/__init__.py
+++ hyperplane_definitions/__init__.py
@@ -1,1 +1,13 @@
+import os
+
+
 OUTPUT_FILES_BASE_DIR = "outputs"
+
+
+def get_job_id():
+    job_id = os.environ.get('HYPERPLANE_JOB_ID')
+
+    if not job_id:
+        print("Warning: job_id is not set, returning None. If needed you can set it via the HYPERPLANE_JOB_ID environment variable")
+
+    return job_id
